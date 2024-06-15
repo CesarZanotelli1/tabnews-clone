53cc6bd2ced2d741590909fe4dc8329c763a0be1
César Augusto Matiello Zanotelli <110305935+CesarZanotelli1@users.noreply.github.com>
2024-06-15 21:43:03 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "alunos do curso.dev" });
+  const version = await database.query("SHOW server_version");
+  const databaseVersionValue = version.rows[0].server_version;
+
+  const databaseMaxCon = await database.query("SHOW max_connections;");
+  const databaseMaxConValue = databaseMaxCon.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionValue =
+    databaseOpenConnectionResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConValue),
+        opened_connections: databaseOpenConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
